f60247e97bc8bafaf412ac94bacb1a8df2c8218e
Jose Marroquin <jmmarroquinr@correo.url.edu.gt>
2022-08-21 00:54:38 -0600

    applicant and AVL classes added

---
+++ TalentHubLab1/Applicant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentHubLab1
+{
+    public class Applicant
+    {
+        public string Name { get; set; }
+        public string DPI { get; set; }
+        public string DateBirth { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
